36609b47bf7b774fb8b8af84143648b0f28be9bf
Pranjal <happybarnwal007@gmail.com>
2024-03-09 04:15:20 +0530

    started next js

---
+++ next_project_w14/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return(
+    <div className="bg-black text-3xl text-white h-[100vh]">
+      Hello there
+    </div>
+  );
+}
---
+++ next_project_w14/app/signup/page.tsx
@@ -0,0 +1,7 @@
+export default function Signup() {
+    return(
+      <div className="bg-black text-3xl text-white h-[100vh]">
+        Hello there from sign up page
+      </div>
+    );
+  }
